3ff2ca31f0448f792596c28e2e0ec3bc5135f32d
xosblo91 <xosblo91@gmail.com>
2024-12-01 15:55:24 +0100

    hej

---
+++ lib/aoc24day1.ex
@@ -0,0 +1,40 @@
+defmodule AOC24Day1 do
+  def part1 do
+    {list1, list2} = read()
+
+    sorted_list1 = Enum.sort(list1)
+    sorted_list2 = Enum.sort(list2)
+
+    sum =
+      Enum.zip(sorted_list1, sorted_list2)
+      |> Enum.reduce(0, fn {val1, val2}, acc -> acc + abs(val1 - val2) end)
+
+    IO.puts("Sum of differences: #{sum}")
+  end
+
+  def part2 do
+    {list1, list2} = read()
+
+    sum =
+      Enum.reduce(list1, 0, fn num, acc ->
+        count_in_list2 = Enum.count(list2, &(&1 == num))
+        acc + num * count_in_list2
+      end)
+
+    IO.puts("Sum of multiplications: #{sum}")
+  end
+
+  def read do
+    file_path = "input_day01.txt"
+
+    {list1, list2} =
+      File.read!(file_path)
+      |> String.split("\n", trim: true)
+      |> Enum.reduce({[], []}, fn line, {list1, list2} ->
+        [left, right] = String.split(line, ~r/\s+/, trim: true)
+        {list1 ++ [String.to_integer(left)], list2 ++ [String.to_integer(right)]}
+      end)
+
+    {list1, list2}
+  end
+end
---
+++ test/aoc24_test.exs
@@ -0,0 +1,11 @@
+defmodule AOC24Day1Test do
+  use ExUnit.Case
+  doctest AOC24Day1
+
+  test "part1" do
+    AOC24Day1.part1()
+  end
+  test "part2" do
+    AOC24Day1.part2()
+  end
+end
